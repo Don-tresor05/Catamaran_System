54f7a804e24c37fd48892c918388132395b9ac39
Don-tresor05 <dontresor922@gmail.com>
2026-03-11 19:33:21 +0200

    config: update Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors:{
-        dark: {
-          8: '#131316',
-          12: '#1C1C21',
-        },
-        grey: {
-          70: '#AFB0B6',
-        },
-        fontFamily: {
-          manrope: ['Manrope', 'sans-serif'],
-        },
-        spacing: {
-          '18': '4.5rem',
-          '62': '15.5rem',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-}
-
+};
